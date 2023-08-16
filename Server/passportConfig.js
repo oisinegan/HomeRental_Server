c5444c59838916f6ea9a1b66d4cd17649257b20f
oisinegan <oisinegan00@gmail.com>
2023-08-16 14:07:47 +0100

    Update passportConfig.js

--- Server/passportConfig.js
+++ Server/passportConfig.js
@@ -30,16 +30,20 @@ module.exports = function (passport) {
   passport.deserializeUser((id, done) => {
     const sql = "SELECT * FROM Landlord WHERE idLandlord = '" + id + "'";
     connection.query(sql, (err, rows, fields) => {
-      if (err) throw err;
+      if (err) {
+        console.log("Error fetching user data:", err);
+        return done(err); // Pass the error to the done callback
+      }
+      if (rows.length === 0) {
+        console.error("User not found in database");
+        return done(null, false); // User not found
+      }
       const userInfo = {
         id: rows[0].idLandlord,
         Name: rows[0].Name,
         email: rows[0].username,
       };
-      console.log("ID(PP config): " + rows[0].idLandlord);
-      console.log("Name(PP config): " + rows[0].Name);
-      console.log("email(PP config): " + rows[0].username);
-      console.log("USER INFO(PP config): " + userInfo);
+      console.log("User info (PP config):", JSON.stringify(userInfo));
       done(null, userInfo);
     });
   });
