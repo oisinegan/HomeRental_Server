a57f094e8a28d833ce09fe14290909b86b1a0c57
oisinegan <oisinegan00@gmail.com>
2023-08-16 12:17:22 +0100

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://home-rental-client.vercel.app/",
     credentials: true,
   })
 );
